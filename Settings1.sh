569e925f9e76d07176db1cb10f9ffdd9416ae935
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2023-06-15 08:32:05 +0900

    Linux Ver

---
+++ Settings1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+clear
+echo TaxFWTools
+echo TaxLibFW
+echo MyQuntom
+echo MyHtml
+echo "# ======= [ Tax0787Reborn's REPO ] ======= #"
+echo PyTry
+echo TaxPips
+echo TaxLib310PlugIn
+echo "-"
+echo "==============================================="
+echo "-"
+ls
+echo "input User and Repo""
+python TaxGitClone.py
